7a432dd01f146dc95aca8940441e63d582aff38c
steven_savarin <srssdesing@gmail.com>
2025-09-13 13:20:52 +0200

    finished

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -60,7 +60,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="es">
+    <html lang="en">
       <body className={cn('font-sans antialiased', outfit.variable)}>
         <AppContextProvider>
           <Lenis>
